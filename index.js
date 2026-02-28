b1931c0a74224730b0f2edf95123d36f578c44eb
caritascharityfund-commits <caritascharityfund@gmail.com>
2026-02-28 14:56:46 -0700

    Add files via upload
    First upload -- all progress so far

---
+++ index.js
@@ -0,0 +1,4 @@
+const greetingElement = document.getElementById("greeting");
+greetingElement.styletextAlign + "center";
+greetingElement.style.marginTop = "50px";
+const username = window.prompt("What is your name?", "Jane")
