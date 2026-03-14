dcdc1a66ecbd3ee77575beb49d6f03ea3ab314fd
caritascharityfund-commits <caritascharityfund@gmail.com>
2026-03-14 08:02:33 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const greetingElement = document.getElementById("greeting");
 greetingElement.styletextAlign + "center";
 greetingElement.style.marginTop = "50px";
-const username = window.prompt("What is your name?", "Welcome to my portfolio!");
-greetingElement.textContent = `Hello, ${username}! Welcome to my portfolio!`; f
+const username = window.prompt("Ready to see something cool?", "Welcome to my portfolio!");
+
